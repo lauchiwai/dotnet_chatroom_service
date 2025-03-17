a4c1f48f77f1b343a2713bf6ad7dc342df24b440
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-17 22:43:57 +0800

    Initial commit for .NET 8 project

---
+++ Common/Dto/ResultDto.cs
@@ -0,0 +1,43 @@
+﻿namespace Common.Dto;
+public class ResultDTO
+{
+    /// <summary>
+    /// 表示操作是否成功
+    /// </summary>
+    public bool IsSuccess { get; set; }
+
+    /// <summary>
+    /// 操作結果的訊息
+    /// </summary>
+    public string Message { get; set; }
+
+    /// <summary>
+    /// 操作結果的資料
+    /// </summary>
+    public object Data { get; set; }
+
+    /// <summary>
+    /// 錯誤訊息
+    /// </summary>
+    public string ErrorMessage { get; set; }
+
+    /// <summary>
+    /// 預設建構函式
+    /// </summary>
+    public ResultDTO( ) {}
+
+    /// <summary>
+    /// 帶有初始化參數的建構函式
+    /// </summary>
+    /// <param name="isSuccess">是否成功</param>
+    /// <param name="message">訊息</param>
+    /// <param name="data">資料</param>
+    /// <param name="errorMessage">錯誤訊息</param>
+    public ResultDTO(bool isSuccess, string message, object data = null, string errorMessage = null)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+        Data = data;
+        ErrorMessage = errorMessage;
+    }
+}
---
+++ Repositories/MyDbContext/MyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Common.Models;
+
+namespace Repositories.MyDbContext;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Authenticate> Authenticates { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Authenticate>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authenti__3213E83FF2FA5720");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
