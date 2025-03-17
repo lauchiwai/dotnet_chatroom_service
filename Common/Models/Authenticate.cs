28491aec577cd2386db53b21e0a81c0afdea9633
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-17 22:43:57 +0800

    Initial commit for .NET 8 project

---
+++ Common/Models/Authenticate.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Common.Models;
+
+[Table("Authenticate")]
+public partial class Authenticate
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("user_name")]
+    [StringLength(50)]
+    public string? UserName { get; set; }
+
+    [Column("pw")]
+    [StringLength(50)]
+    public string? Pw { get; set; }
+
+    [Column("refresh_token")]
+    [StringLength(50)]
+    public string? RefreshToken { get; set; }
+
+    [Column("refresh_token_expiry_time")]
+    public DateOnly? RefreshTokenExpiryTime { get; set; }
+}
