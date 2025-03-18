708205f95f180597ebe8353df8d647d8af9d9780
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-18 15:59:37 +0800

    feat: 注冊功能

---
+++ Services/Interfaces/IAuthenticateService.cs
@@ -0,0 +1,15 @@
+﻿using Common.Dto;
+using Common.Params;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Services.Interfaces;
+
+public interface IAuthenticateService
+{
+    /// <summary>
+    /// 注冊
+    /// </summary>
+    /// <param name="registerFrom"></param>
+    /// <returns></returns>
+    Task<ResultDTO> Register([FromBody] RegisterParams registerFrom);
+}
