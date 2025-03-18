304ab9a48b8a3118ad1bcd76173ccc987a3c7110
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-18 16:00:13 +0800

    feat: 登錄以及刷新 token 功能

--- Services/Interfaces/IAuthenticateService.cs
+++ Services/Interfaces/IAuthenticateService.cs
@@ -12,4 +12,17 @@ public interface IAuthenticateService
     /// <param name="registerFrom"></param>
     /// <returns></returns>
     Task<ResultDTO> Register([FromBody] RegisterParams registerFrom);
+
+    /// <summary>
+    /// 登入
+    /// </summary>
+    /// <returns></returns>
+    Task<ResultDTO> Login([FromBody] LoginParams loginFrom);
+
+    /// <summary>
+    /// 更新 Token
+    /// </summary>
+    /// <param name="refreshToken"></param>
+    /// <returns></returns>
+    Task<ResultDTO> RefreshToken(string refreshToken);
 }
