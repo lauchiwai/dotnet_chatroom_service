839c7b68a9a57acbdf35f00ec44c9726975340c3
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-18 16:00:48 +0800

    feat: 新增 chat session 表單

---
+++ Common/Models/ChatSession.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Common.Models;
+
+[Table("Chat_Session")]
+public partial class ChatSession
+{
+    [Key]
+    [Column("SessionID")]
+    public int SessionId { get; set; }
+
+    [StringLength(50)]
+    public string? SessionName { get; set; }
+
+    [StringLength(50)]
+    public string UserId { get; set; } = null!;
+
+    [Column(TypeName = "datetime")]
+    public DateTime UpdateTime { get; set; }
+}
