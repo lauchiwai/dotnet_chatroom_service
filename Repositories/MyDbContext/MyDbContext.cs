548c11a0607a70d6487bf78c52ea01e93f1bb2b0
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-18 16:00:48 +0800

    feat: 新增 chat session 表單

--- Repositories/MyDbContext/MyDbContext.cs
+++ Repositories/MyDbContext/MyDbContext.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Common.Models;
+﻿using Common.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repositories.MyDbContext;
 
@@ -16,11 +16,18 @@ public partial class MyDbContext : DbContext
 
     public virtual DbSet<Authenticate> Authenticates { get; set; }
 
+    public virtual DbSet<ChatSession> ChatSessions { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Authenticate>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__Authenti__3213E83FF2FA5720");
+        });
+
+        modelBuilder.Entity<ChatSession>(entity =>
+        {
+            entity.HasKey(e => e.SessionId).HasName("PK__Chat_Ses__C9F4927095D5C284");
         });
 
         OnModelCreatingPartial(modelBuilder);
