af77d56918d447407724c864601ff3fcb7c3491a
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-18 15:52:47 +0800

    feat : swagger 自定義

--- dotnet_chatroom_service/Program.cs
+++ dotnet_chatroom_service/Program.cs
@@ -8,12 +8,8 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // db services 
 builder.Services.AddDbContext<MyDbContext>(options =>
@@ -39,6 +35,35 @@ builder.Services.AddAuthentication(options =>
         ValidAudience = jwtConfig["Audience"],
         ValidateLifetime = true,
     };
+});
+
+// swagger services 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DotNet Chatroom API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
 });
 var app = builder.Build();
 
