e4d198b880f123268c8359642e4b957113dc8f62
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-18 15:52:25 +0800

    feat: jwt 服務注冊和基本設定

--- dotnet_chatroom_service/Program.cs
+++ dotnet_chatroom_service/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Repositories.MyDbContext;
+using Services.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +15,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// db services 
 builder.Services.AddDbContext<MyDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// jwt services
+var jwtConfig = builder.Configuration.GetSection("JwtConfig");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig["SecretKey"])),
+        ValidateIssuer = true,
+        ValidIssuer = jwtConfig["Issuer"],
+        ValidateAudience = true,
+        ValidAudience = jwtConfig["Audience"],
+        ValidateLifetime = true,
+    };
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
