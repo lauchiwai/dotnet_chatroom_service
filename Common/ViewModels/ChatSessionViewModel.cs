94d7ac7b3d5f6b33dba8c8c25978903b51ba5e7c
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-19 14:29:53 +0800

    feat: 新增 聊天會話功能

---
+++ Common/ViewModels/ChatSessionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.ViewModels;
+
+public class ChatSessionViewModel
+{
+    /// <summary>
+    /// 聊天會話id
+    /// </summary>
+    public int SessionId { get; set; }
+
+    /// <summary>
+    /// 聊天會話Name
+    /// </summary>
+    public string SessionName { get; set; } = null!;
+}
---
+++ Services/Interfaces/IChatSessionService.cs
@@ -0,0 +1,13 @@
+﻿using Common.Dto;
+
+namespace Services.Interfaces;
+
+public interface IChatSessionService
+{
+    /// <summary>
+    /// 創建聊天會話
+    /// </summary>
+    /// <param name="userTimeZoneId"></param>
+    /// <returns></returns>
+    public Task<ResultDTO> GenerateChatSession(string userTimeZoneId = "Asia/Hong_Kong");
+}
