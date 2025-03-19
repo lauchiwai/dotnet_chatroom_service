b5311d4eaba3f94d29f110cbad4aac86af3b07f8
lauchiwai <lauuai5244@yahoo.com.hk>
2025-03-19 14:29:53 +0800

    feat: 新增 聊天會話功能

---
+++ Services/Implementation/ChatSessionService.cs
@@ -0,0 +1,58 @@
+﻿using Common.Dto;
+using Common.Helper.Implementation;
+using Common.Helper.Interface;
+using Common.Models;
+using Common.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Repositories.MyDbContext;
+using Services.Interfaces;
+using System.Globalization;
+
+namespace Services.Implementation;
+
+public class ChatSessionService : IChatSessionService
+{
+    private readonly MyDbContext _context;
+    private readonly IUserHelper _jwtHelper;
+
+    public ChatSessionService(MyDbContext context, IUserHelper jwtHelper)
+    {
+        _context = context;
+        _jwtHelper = jwtHelper;
+    }
+
+    public async Task<ResultDTO> GenerateChatSession(string userTimeZoneId = "Asia/Hong_Kong")
+    {
+        var result = new ResultDTO() { IsSuccess = true };
+        try
+        {
+            var userInfo = _jwtHelper.ParseToken<JwtUserInfo>();
+            var userTimeZone = TimeZoneInfo.FindSystemTimeZoneById(userTimeZoneId);
+            var userLocalTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, userTimeZone);
+            var newChatSession = new ChatSession()
+            {
+                UserId = userInfo.UserId,
+                SessionName = userLocalTime.ToString( "yyyy年MM月dd日HH時mm分", CultureInfo.InvariantCulture ),
+                UpdateTime = DateTime.UtcNow
+            };
+            _context.ChatSessions.Add(newChatSession);
+            await _context.SaveChangesAsync();
+
+            var chatSessionViewModel = new ChatSessionViewModel()
+            {
+                SessionId = newChatSession.SessionId,
+                SessionName = newChatSession.SessionName,
+            };
+
+            result.Data = chatSessionViewModel;
+        }
+        catch (Exception ex)
+        {
+            result.IsSuccess = false;
+            result.Message = ex.Message;
+        }
+
+        return result;
+    }
+
+}
---
+++ dotnet_chatroom_service/Controllers/ChatSessionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace dotnet_chatroom_service.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatSessionController : ControllerBase
+{
+    private readonly IChatSessionService _chatSessionService;
+
+    public ChatSessionController(IChatSessionService chatSessionService)
+    {
+        _chatSessionService = chatSessionService;
+    }
+
+    [HttpPost("GenerateChatSession")]
+    [Authorize]
+    public async Task<IActionResult> GenerateChatSession(string userTimeZoneId = "Asia/Hong_Kong")
+    {
+        var result = await _chatSessionService.GenerateChatSession(userTimeZoneId);
+
+        if (result.IsSuccess)
+            return Ok(result);
+        else
+            return BadRequest(result);
+    }
+}
